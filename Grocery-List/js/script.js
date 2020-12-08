1d9c647d150a7c5d4c51c8a6db10f458f46d13a3
Abhishek Singh <writetoabhishek07@gmail.com>
2020-12-08 18:25:36 +0530

    added the local storage functionality

--- Grocery-List/js/script.js
+++ Grocery-List/js/script.js
@@ -3,17 +3,21 @@ const groceryButton = document.querySelector('.grocery__button');
 const groceryList = document.querySelector('.grocery__list');
 
 groceryButton.addEventListener('click',addGrocery);
+groceryList.addEventListener('click',deleteGrocery);
 
 function addGrocery(e){
+   
     e.preventDefault();
 
     const grocery = document.createElement('div');
     grocery.classList.add("grocery");
 
     const newGrocery = document.createElement('li');
-    newGrocery.innerText = 'cool';
+    newGrocery.innerText = groceryInput.value;
     newGrocery.classList.add('grocery');
     grocery.appendChild(newGrocery);
+
+    localStorageGrocery(groceryInput.value);
 
     const checkedButton = document.createElement('button');
     checkedButton.innerHTML = '<i class = "fas fa-check"></i>';
@@ -22,8 +26,36 @@ function addGrocery(e){
 
     const deleteButton = document.createElement('button');
     deleteButton.innerHTML = '<i class = "fas fa-trash"></i>';
-    deleteButton.classList.add("checked__btn");
+    deleteButton.classList.add("delete__btn");
     grocery.appendChild(deleteButton);
 
     groceryList.appendChild(grocery);
+
+    groceryInput.value="";
+}
+
+function deleteGrocery(e){
+    const item = e.target;
+
+    if(item.classList[0] === "delete__btn"){
+        const grocery = item.parentElement;
+        grocery.remove();
+    }
+
+    if(item.classList[0]==="checked__btn"){
+        const grocery = item.parentElement;
+        grocery.classList.toggle("checked");
+    }
+}
+
+function localStorageGrocery(grocery){
+    let groceries;
+    if(localStorage.getItem('groceries')===null){
+        groceries =[];
+    }
+    else {
+        groceries = JSON.parse(localStorage.getItem('groceries'));
+    }
+    groceries.push(grocery);
+    localStorage.setItem('groceries',JSON.stringify(groceries));
 }
